7748bd77ee856c121eddfd8cc82400c601347912
Vikram Devatha <32735235+vikramdevatha@users.noreply.github.com>
2018-04-27 09:59:14 +0530

    Add files via upload
    Function for cleaning text
    By default, the function removes numbers, removes stop words and does not stem the document
    Usage in local R: 
    
    if (!require(RCurl)) {install.packages("RCurl")}; library(RCurl)
    Text_Clean = getURL("insert raw GIT URL", ssl.verifypeer=FALSE)
    eval(parse(text=Text_Clean))
    Read text into x
    x_clean = Text_Clean(x, remove_numbers=TRUE\FALSE, remove_stopwords = TRUE\FALSE, stem_document = TRUE\FALSE)

---
+++ Text_Clean.R
@@ -0,0 +1,49 @@
+# Function for cleaning text
+# By default, the function removes numbers, removes stop words and does nto stem the document
+# Usage in local R: 
+#   if (!require(RCurl)) {install.packages("RCurl")}; library(RCurl)
+#   Text_Clean = getURL("insert raw GIT URL", ssl.verifypeer=FALSE)
+#   eval(parse(text=Text_Clean))
+#   x_clean = Text_Clean(x, remove_numbers=TRUE\FALSE, remove_stopwords = TRUE\FALSE, stem_document = TRUE\FALSE)
+# Returns clean text
+
+Text_Clean = function(text.input,
+                      remove_numbers = TRUE,
+                      remove_stopwords = TRUE,
+                      stem_document = FALSE) {
+  
+  if (!require(tm)) {install.packages("tm")}; library(tm)
+  if (!require(dplyr)) {install.packages("dplyr")}; library(dplyr)
+  
+  text.input = gsub("<*.?>", " ", text.input) #removing HTML tags
+  text.input = gsub("[^[:alnum:]]", " ", text.input) #removing anythign that is not alphanumeric
+  text.input = iconv(text.input, "latin1", "ASCII", sub=" ") #keep only ASCII characters
+  text.input = tolower(text.input)
+  text.input = stripWhitespace(text.input)
+  text.input = gsub("^\\s+|\\s+$", " ", text.input) #removing space at the beginning and at end
+  
+  if(remove_stopwords){
+    #combining stop words from two sources - GIT and TM package
+    stop_words = readLines("https://raw.githubusercontent.com/vikramdevatha/textan/master/stopwords.txt")
+    stop_words2 = tm::stopwords(kind='en')
+    common_stop_words = unique(c(stop_words, stop_words2))
+    common_stop_words = unique(gsub("'", "", common_stop_words)) #removing the apostrophy
+    
+    text.input %>% 
+      removeWords(common_stop_words) %>% 
+      stripWhitespace() ->
+      text.input
+  }
+  
+  if(remove_numbers){
+    text.input %>%
+      removeNumbers() ->
+      text.input
+  }
+  
+  if(stem_document){
+    text.input = stemDocument(text.input) #stems the document
+  }
+  
+  return(text.input)
+}
