1d2319caf26675a3c48a96698a1a97b175fc1a5b
Vikram Devatha <32735235+vikramdevatha@users.noreply.github.com>
2018-05-04 21:16:25 +0530

    Update Dependencies.R

--- Dependencies.R
+++ Dependencies.R
@@ -1,17 +1,19 @@
 # Dependeancy Libraries for Basic Text Analytics
 
 # Install the required pckages, and load them
-if (!require(udpipe)){install.packages("udpipe")}; library(udpipe)
-if (!require(textrank)){install.packages("textrank")}; library(textrank)
-if (!require(lattice)){install.packages("lattice")}; library(lattice)
-if (!require(igraph)){install.packages("igraph")}; library(igraph)
-if (!require(ggraph)){install.packages("ggraph")}; library(ggraph)
-if (!require(wordcloud)){install.packages("wordcloud")}; library(wordcloud)
-if (!require(RCurl)) {install.packages("RCurl")}; library(RCurl)
-if (!require(tm)) {install.packages("tm")}; library(tm)
-if (!require(dplyr)) {install.packages("dplyr")}; library(dplyr)
-if (!require(tidytext)) {install.packages("tidytext")}; library(tidytext)
-if (!require(tibble)) {install.packages("tibble")}; library(tibble)
-if (!require(tidyverse)) {install.packages("tidyverse")}; library(tidyverse)
-if (!require(tidyr)) {install.packages("tidyr")}; library(tidyr)
-if (!require(reticulate)) {install.packages("reticulate")}; library(reticulate)
+dependency = function{
+  if (!require(udpipe)){install.packages("udpipe")}; library(udpipe)
+  if (!require(textrank)){install.packages("textrank")}; library(textrank)
+  if (!require(lattice)){install.packages("lattice")}; library(lattice)
+  if (!require(igraph)){install.packages("igraph")}; library(igraph)
+  if (!require(ggraph)){install.packages("ggraph")}; library(ggraph)
+  if (!require(wordcloud)){install.packages("wordcloud")}; library(wordcloud)
+  if (!require(RCurl)) {install.packages("RCurl")}; library(RCurl)
+  if (!require(tm)) {install.packages("tm")}; library(tm)
+  if (!require(dplyr)) {install.packages("dplyr")}; library(dplyr)
+  if (!require(tidytext)) {install.packages("tidytext")}; library(tidytext)
+  if (!require(tibble)) {install.packages("tibble")}; library(tibble)
+  if (!require(tidyverse)) {install.packages("tidyverse")}; library(tidyverse)
+  if (!require(tidyr)) {install.packages("tidyr")}; library(tidyr)
+  if (!require(reticulate)) {install.packages("reticulate")}; library(reticulate)
+  }
